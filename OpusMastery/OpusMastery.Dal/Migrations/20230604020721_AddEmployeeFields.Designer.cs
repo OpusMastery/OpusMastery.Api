809db009e300933e43098a2b71e89ab48ffe8d69
Rostyslav Nakarlovich <zvesdochyot@gmail.com>
2023-06-04 05:09:30 +0300

    Added migration for Employee domain

--- OpusMastery/OpusMastery.Dal/Migrations/20230604020721_AddEmployeeFields.Designer.cs
+++ OpusMastery/OpusMastery.Dal/Migrations/20230604020721_AddEmployeeFields.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OpusMastery.Dal.Contexts;
@@ -11,9 +12,11 @@ using OpusMastery.Dal.Contexts;
 namespace OpusMastery.Dal.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230604020721_AddEmployeeFields")]
+    partial class AddEmployeeFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OpusMastery/OpusMastery.Dal/Migrations/20230604020721_AddEmployeeFields.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpusMastery.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmployeeFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Companies");
+
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Companies",
+                newName: "ContactPhone");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Companies",
+                newName: "ContactEmail");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactEmail",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPhone",
+                table: "Employees",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DepartmentName",
+                table: "Employees",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "JoiningDate",
+                table: "Employees",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactEmail",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "ContactPhone",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentName",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "JoiningDate",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "ContactPhone",
+                table: "Companies",
+                newName: "Status");
+
+            migrationBuilder.RenameColumn(
+                name: "ContactEmail",
+                table: "Companies",
+                newName: "Email");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Companies",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
